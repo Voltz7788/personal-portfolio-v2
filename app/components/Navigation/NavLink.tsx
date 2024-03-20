d2a0140258ad16cc251aef5b1da02b16c56cbc80
Yannis <yannismadu777@gmail.com>
2024-03-20 09:34:22 +0000

    feat: add navbar animation

--- app/components/Navigation/NavLink.tsx
+++ app/components/Navigation/NavLink.tsx
@@ -1,15 +1,36 @@
 import Link from "next/link";
 import { Url } from "next/dist/shared/lib/router/router";
+import { motion } from "framer-motion";
 
-export default function NavLink({ href, title }: { href: Url; title: string }) {
+export default function NavLink({
+  href,
+  title,
+  index,
+}: {
+  href: Url;
+  title: string;
+  index: number;
+}) {
   return (
-    <Link
-      href={href}
-      className="grid place-content-center h-24 hover:bg-foreground transition-all 
+    <Link href={href}>
+      <div
+        className="grid place-content-center h-24 hover:bg-foreground transition-all 
       duration-100 border-r-2 border-background hover:border-secondary font-extralight 
       hover:text-copy text-copy-lighter text-lg"
-    >
-      <p className="rotate-90 ">{title}</p>
+      >
+        <motion.p
+          initial={{ x: "-150%", rotate: "90deg" }}
+          animate={{ x: "0%" }}
+          transition={{
+            ease: "easeInOut",
+            delay: 0.1 * index + 0.2,
+            duration: 0.5,
+          }}
+          viewport={{ once: true }}
+        >
+          {title}
+        </motion.p>
+      </div>
     </Link>
   );
 }
--- app/components/Navigation/Navbar.tsx
+++ app/components/Navigation/Navbar.tsx
@@ -2,10 +2,23 @@
 import Link from "next/link";
 import React from "react";
 import NavLink from "./NavLink";
+import { motion } from "framer-motion";
+
+const navLinks: { title: string; href: string }[] = [
+  { title: "About", href: "#about" },
+  { title: "Projects", href: "#projects" },
+  { title: "Exp.", href: "#experience" },
+  { title: "Contact", href: "#contact" },
+];
 
 export default function Navbar() {
   return (
-    <nav className="h-screen w-16 fixed top-0 bg-background ">
+    <motion.nav
+      initial={{ x: "-100%" }}
+      animate={{ x: "0%" }}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
+      className="h-screen w-16 fixed top-0 bg-background"
+    >
       <div className="flex flex-col">
         <Link
           href={"#hero"}
@@ -13,12 +26,16 @@ export default function Navbar() {
         >
           L
         </Link>
-        <NavLink href={"#about"} title="About" />
-        <NavLink href={"#projects"} title="Projects" />
-        <NavLink href={"#experience"} title="Exp." />
-        <NavLink href={"#contact"} title="Contact" />
+        {navLinks.map((link, index) => (
+          <NavLink
+            key={link.title}
+            title={link.title}
+            href={link.href}
+            index={index}
+          />
+        ))}
         <div className="h-screen border-r-2 border-background" />
       </div>
-    </nav>
+    </motion.nav>
   );
 }
