0937908a298658f23d7449b1987b33ef74d9e420
Yannis <yannismadu777@gmail.com>
2024-03-20 06:15:00 +0000

    feat: set up basic tailwind themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,33 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "hsla(var(--primary))",
+        "primary-content": "hsla(var(--primary-content))",
+        "primary-dark": "hsla(var(--primary-dark))",
+        "primary-light": "hsla(var(--primary-light))",
+
+        secondary: "hsla(var(--secondary))",
+        "secondary-content": "hsla(var(--secondary-content))",
+        "secondary-dark": "hsla(var(--secondary-dark))",
+        "secondary-light": "hsla(var(--secondary-light)",
+
+        background: "hsla(var(--background))",
+        foreground: "hsla(var(--foreground))",
+        border: "hsla(var(--border))",
+
+        copy: "hsla(var(--copy))",
+        "copy-light": "hsla(var(--copy-light))",
+        "copy-lighter": "hsla(var(--copy-dark))",
+
+        success: "hsla(var(--success))",
+        warning: "hsla(var(--warning))",
+        error: "hsla(var(--error))",
+
+        "success-content": "hsla(var(--success-content))",
+        "warning-content": "hsla(var(--warning-content))",
+        "error-content": "hsla(var(--error-content))",
+      },
     },
   },
   plugins: [],
