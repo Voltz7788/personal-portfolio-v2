a3d2b3abd77c60d5864fff08f8373d117d6e998a
Yannis <yannismadu777@gmail.com>
2024-03-20 08:11:31 +0000

    feat: add social links and resume button

--- app/components/Navigation/SocialLinks.tsx
+++ app/components/Navigation/SocialLinks.tsx
@@ -8,19 +8,19 @@ export default function SocialLinks() {
   return (
     <div className="flex justify-between w-36">
       <Link href={"https://www.linkedin.com/in/yannismadu/"} target="_blank">
-        <FaLinkedin className="text-2xl text-primary-light hover:text-primary transition-colors" />
+        <FaLinkedin className="text-2xl text-copy-light hover:text-primary transition-colors" />
       </Link>
       <Link href={"https://github.com/Voltz7788"} target="_blank">
-        <FaGithub className="text-2xl text-primary-light hover:text-primary transition-colors" />
+        <FaGithub className="text-2xl text-copy-light hover:text-primary transition-colors" />
       </Link>
       <Link
         href={"https://www.youtube.com/channel/UCgxPKeb3nrpenK3XGvzp2tA"}
         target="_blank"
       >
-        <FaYoutube className="text-2xl text-primary-light hover:text-primary transition-colors" />
+        <FaYoutube className="text-2xl text-copy-light hover:text-primary transition-colors" />
       </Link>
       <Link href={"https://www.tiktok.com/@yannismadu?lang=en"} target="_blank">
-        <FaTiktok className="text-2xl text-primary-light hover:text-primary transition-colors" />
+        <FaTiktok className="text-2xl text-copy-light hover:text-primary transition-colors" />
       </Link>
     </div>
   );
--- app/components/Navigation/TopLinks.tsx
+++ app/components/Navigation/TopLinks.tsx
@@ -1,9 +1,35 @@
+"use client";
 import SocialLinks from "./SocialLinks";
+import { motion } from "framer-motion";
 
 export default function TopLinks() {
   return (
-    <div className="fixed flex items-center px-10 ml-16 h-20 w-full background-blur-2xl">
+    <div className="fixed flex items-center justify-between w-full mx-16 pl-10 pr-24 h-20 backdrop-blur-sm">
       <SocialLinks />
+      <motion.a
+        whileHover={"hover"}
+        initial={{ scale: 1 }}
+        variants={buttonMotion}
+        transition={{ ease: "easeInOut", duration: 0.08 }}
+        className="relative border border-secondary px-4 py-2 rounded text-lg text-secondary 
+        hover:text-foreground ml-auto transition-colors duration-150 cursor-pointer"
+      >
+        <motion.div
+          initial={{ scaleY: 0, scaleX: 0 }}
+          variants={colorMotion}
+          transition={{ ease: "easeInOut", duration: 0.15 }}
+          className="-z-10 absolute top-0 left-0 w-full h-full origin-top-left bg-secondary"
+        />
+        My resume
+      </motion.a>
     </div>
   );
 }
+
+const colorMotion = {
+  hover: { scaleY: 1, scaleX: 1 },
+};
+
+const buttonMotion = {
+  hover: { scale: 1.1, rotate: "1deg" },
+};
